5383a6fad9fdb2d2e18f6a791c2a31cac0ec7414
Bronson Zgeb <bronson.zgeb@unity3d.com>
2021-07-14 20:58:07 -0400

    Renamed project

--- Assets/PixelatePostProcessing/PixelateRunner.cs
+++ Assets/PixelatePostProcessing/PixelateRunner.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 
-public class PixelizeRunner : MonoBehaviour
+public class PixelateRunner : MonoBehaviour
 {
-    public ComputeShader PixelizeComputeShader;
+    public ComputeShader PixelateComputeShader;
     [Range(2, 40)] public int BlockSize = 3;
 
     int _screenWidth;
@@ -35,12 +35,12 @@ public class PixelizeRunner : MonoBehaviour
     {
         Graphics.Blit(src, _renderTexture);
 
-        var mainKernel = PixelizeComputeShader.FindKernel("Pixelize");
-        PixelizeComputeShader.SetInt("_BlockSize", BlockSize);
-        PixelizeComputeShader.SetInt("_ResultWidth", _renderTexture.width);
-        PixelizeComputeShader.SetInt("_ResultHeight", _renderTexture.height);
-        PixelizeComputeShader.SetTexture(mainKernel, "Result", _renderTexture);
-        PixelizeComputeShader.Dispatch(mainKernel,
+        var mainKernel = PixelateComputeShader.FindKernel("Pixelate");
+        PixelateComputeShader.SetInt("_BlockSize", BlockSize);
+        PixelateComputeShader.SetInt("_ResultWidth", _renderTexture.width);
+        PixelateComputeShader.SetInt("_ResultHeight", _renderTexture.height);
+        PixelateComputeShader.SetTexture(mainKernel, "Result", _renderTexture);
+        PixelateComputeShader.Dispatch(mainKernel,
             Mathf.CeilToInt(_renderTexture.width / (float)BlockSize / 8f),
             Mathf.CeilToInt(_renderTexture.height / (float)BlockSize / 8f),
             1);
